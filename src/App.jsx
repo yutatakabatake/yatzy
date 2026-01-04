07cc24651fc2779fc023d5d742969d27eef1ed2b
yutatakabatake <yutatakabatake@gmail.com>
2026-01-04 23:47:17 +0900

    Change score structure manegement

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,34 @@ import './App.css'
 import RollButton from './components/RollButton'
 import Dicearea from './components/Dicearea'
 import ScoreBoard from './components/ScoreBoard'
-import { calcLowerScores, calcUpperScores } from './logic/calculate'
+import { calcScores } from './logic/calculate'
 
 function App() {
   const [isHolds, setIsHolds] = useState(Array(5).fill(false));
   const [faces, setFaces] = useState(Array(5).fill(0));
   const [count, setCount] = useState(3);
-  const [upperScores, setUpperScores] = useState([
+  const [scores, setScores] = useState([
     { key: "Ones", score: null },
     { key: "Twos", score: null },
     { key: "Threes", score: null },
     { key: "Fours", score: null },
     { key: "Fives", score: null },
     { key: "Sixes", score: null },
-  ]);
-  const [lowerScores, setLowerScores] = useState([
     { key: "Three", score: null },
     { key: "Four", score: null },
     { key: "HullHouse", score: null },
     { key: "SmallStraight", score: null },
     { key: "LargeStraight", score: null },
     { key: "Chance", score: null },
-    { key: "Yatzy", score: null },
+    { key: "Yatzy", score: null }
   ]);
-  const [upperPossibleScores, setUpperPossibleScores] = useState([
+  const [possibleScores, setPossibleScores] = useState([
     { key: "Ones", score: 0 },
     { key: "Twos", score: 0 },
     { key: "Threes", score: 0 },
     { key: "Fours", score: 0 },
     { key: "Fives", score: 0 },
     { key: "Sixes", score: 0 },
-  ]);
-  const [lowerPossibleScores, setLowerPossibleScores] = useState([
     { key: "Three", score: 0 },
     { key: "Four", score: 0 },
     { key: "HullHouse", score: 0 },
@@ -64,31 +60,15 @@ function App() {
     setFaces(newFaces);
     setCount(count - 1);
 
-    const newUpperPossibleScores = calcUpperScores(newFaces);
-    setUpperPossibleScores(newUpperPossibleScores);
-
-    const newLowerPossibleScores = calcLowerScores(newFaces);
-    setLowerPossibleScores(newLowerPossibleScores);
+    const newPossibleScores = calcScores(newFaces);
+    setPossibleScores(newPossibleScores);
   }
 
   function handleSelect(key) {
-    if (key === "Ones" ||
-      key === "Twos" ||
-      key === "Threes" ||
-      key === "Fours" ||
-      key === "Fives" ||
-      key === "Sixes"
-    ) {
-      const newUpperScores = upperScores.map((row, i) => (
-        row.key === key ? { ...row, score: upperPossibleScores[i].score } : row
-      ));
-      setUpperScores(newUpperScores);
-    } else {
-      const newLowerScores = lowerScores.map((row, i) => (
-        row.key === key ? { ...row, score: lowerPossibleScores[i].score } : row
-      ));
-      setLowerScores(newLowerScores);
-    }
+    const newScores = scores.map((row, i) => (
+      row.key === key ? { ...row, score: possibleScores[i].score } : row
+    ));
+    setScores(newScores);
     initRound();
   }
 
@@ -96,22 +76,20 @@ function App() {
     setIsHolds(Array(5).fill(false));
     setFaces(Array(5).fill(0));
     setCount(3);
-    setUpperPossibleScores([
+    setPossibleScores([
       { key: "Ones", score: 0 },
       { key: "Twos", score: 0 },
       { key: "Threes", score: 0 },
       { key: "Fours", score: 0 },
       { key: "Fives", score: 0 },
       { key: "Sixes", score: 0 },
-    ]);
-    setLowerPossibleScores([
       { key: "Three", score: 0 },
       { key: "Four", score: 0 },
       { key: "HullHouse", score: 0 },
       { key: "SmallStraight", score: 0 },
       { key: "LargeStraight", score: 0 },
       { key: "Chance", score: 0 },
-      { key: "Yatzy", score: 0 },
+      { key: "Yatzy", score: 0 }
     ]);
   }
 
@@ -129,14 +107,13 @@ function App() {
           <Dicearea faces={faces} isHolds={isHolds} handleHold={handleHold} isInit={count === 3} />
           <RollButton count={count} handleRoll={handleRoll} />
         </div>
-        <ScoreBoard upperScores={upperScores}
-          lowerScores={lowerScores}
-          upperPossibleScores={upperPossibleScores}
-          lowerPossibleScores={lowerPossibleScores}
+        <ScoreBoard upperScores={scores.slice(0, 6)}
+          lowerScores={scores.slice(6)}
+          upperPossibleScores={possibleScores.slice(0, 6)}
+          lowerPossibleScores={possibleScores.slice(6)}
           handleSelect={handleSelect} />
       </div>
     </div>
-
   )
 }
 
