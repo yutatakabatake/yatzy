5854174b416d4c8c0d427d7927397c72237f4859
yutatakabatake <yutatakabatake@gmail.com>
2026-01-05 18:46:48 +0900

    Add displaying all player's score board

--- src/Game.jsx
+++ src/Game.jsx
@@ -9,21 +9,7 @@ function Game({ players }) {
   const [isHolds, setIsHolds] = useState(Array(5).fill(false));
   const [faces, setFaces] = useState(Array(5).fill(0));
   const [count, setCount] = useState(3);
-  const [scores, setScores] = useState([
-    { key: "Ones", score: null },
-    { key: "Twos", score: null },
-    { key: "Threes", score: null },
-    { key: "Fours", score: null },
-    { key: "Fives", score: null },
-    { key: "Sixes", score: null },
-    { key: "Three", score: null },
-    { key: "Four", score: null },
-    { key: "HullHouse", score: null },
-    { key: "SmallStraight", score: null },
-    { key: "LargeStraight", score: null },
-    { key: "Chance", score: null },
-    { key: "Yatzy", score: null }
-  ]);
+  const [scores, setScores] = useState(createInitialScores(players));
   const [possibleScores, setPossibleScores] = useState([
     { key: "Ones", score: 0 },
     { key: "Twos", score: 0 },
@@ -107,14 +93,55 @@ function Game({ players }) {
           <Dicearea faces={faces} isHolds={isHolds} handleHold={handleHold} isInit={count === 3} />
           <RollButton count={count} handleRoll={handleRoll} />
         </div>
-        <ScoreBoard upperScores={scores.slice(0, 6)}
-          lowerScores={scores.slice(6)}
-          upperPossibleScores={possibleScores.slice(0, 6)}
-          lowerPossibleScores={possibleScores.slice(6)}
-          handleSelect={handleSelect} />
+        {scores.map((playerScores, index) => (
+          <ScoreBoard
+            key={index}
+            upperScores={playerScores.slice(0, 6)}
+            lowerScores={playerScores.slice(6)}
+            upperPossibleScores={possibleScores.slice(0, 6)}
+            lowerPossibleScores={possibleScores.slice(6)}
+            handleSelect={handleSelect}
+          />
+        ))}
       </div>
     </div>
   )
+}
+
+function createInitialScores(players) {
+  return (Array(players).fill([
+    { key: "Ones", score: null },
+    { key: "Twos", score: null },
+    { key: "Threes", score: null },
+    { key: "Fours", score: null },
+    { key: "Fives", score: null },
+    { key: "Sixes", score: null },
+    { key: "Three", score: null },
+    { key: "Four", score: null },
+    { key: "HullHouse", score: null },
+    { key: "SmallStraight", score: null },
+    { key: "LargeStraight", score: null },
+    { key: "Chance", score: null },
+    { key: "Yatzy", score: null }
+  ]));
+}
+
+function createInitialPossibleScores(players) {
+  return (Array(players).fill([
+    { key: "Ones", score: 0 },
+    { key: "Twos", score: 0 },
+    { key: "Threes", score: 0 },
+    { key: "Fours", score: 0 },
+    { key: "Fives", score: 0 },
+    { key: "Sixes", score: 0 },
+    { key: "Three", score: 0 },
+    { key: "Four", score: 0 },
+    { key: "HullHouse", score: 0 },
+    { key: "SmallStraight", score: 0 },
+    { key: "LargeStraight", score: 0 },
+    { key: "Chance", score: 0 },
+    { key: "Yatzy", score: 0 }
+  ]));
 }
 
 export default Game
