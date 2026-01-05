03aaad05ffcabfa89f45ab453f2fa87722b65697
yutatakabatake <yutatakabatake@gmail.com>
2026-01-05 19:50:42 +0900

    Add style for home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom"
+import "./Home.css"
 
 function SelectMode() {
     return (
-        <>
-            <Link to={"/one"}>SOLO</Link>
-            <Link to={"/two"}>VS</Link>
-        </>
+        <div className="select-mode">
+            <h1 className="title">YATZY</h1>
+
+            <div className="mode-buttons">
+                <Link to="/one" className="mode-button">
+                    SOLO
+                </Link>
+
+                <Link to="/two" className="mode-button">
+                    VS
+                </Link>
+            </div>
+        </div>
     )
 }
 
