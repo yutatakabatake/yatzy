8ff23a74dbf5fed48d3fa6adddb45165b6a9bbf9
yutatakabatake <yutatakabatake@gmail.com>
2026-01-30 19:01:46 +0900

    Move images

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,12 +1,12 @@
 import './Dice.css'
 
 const FACES = [
-    "src/assets/dice1.png",
-    "src/assets/dice2.png",
-    "src/assets/dice3.png",
-    "src/assets/dice4.png",
-    "src/assets/dice5.png",
-    "src/assets/dice6.png"
+    "public/dice1.png",
+    "public/dice2.png",
+    "public/dice3.png",
+    "public/dice4.png",
+    "public/dice5.png",
+    "public/dice6.png"
 ];
 
 type Props = {
